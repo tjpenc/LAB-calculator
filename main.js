946b341e60e9e4b45105fa5663884d27ff0ffbd1
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-01-20 23:47:32 -0500

    cleaned up css

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
   console.log("Yeah mom I vape")
+
+  /*SOFT CODING
+    When click a number, store that number
+    when click an operator, store that operator
+    when click the clear button, clear all the values
+    store previous result, but do not allow previous result to influnce equation unless called
+    when click enter, give result and clear the que */
